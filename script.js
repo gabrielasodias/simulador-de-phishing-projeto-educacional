dcc697e1934e3296e38679384df3ee944f3d2312
Gabriela <gabrieladiasofc@gmail.com>
2025-05-31 15:27:04 -0300

    Arquivo JS

---
+++ script.js
@@ -0,0 +1,11 @@
+function salvarDados() {
+    const usuario = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
+
+    console.clear();
+    console.log("🔒 DADOS CAPTURADOS (simulação):")
+    console.log("Usuário:", usuario);
+    console.log("Senha:", senha);
+
+    alert("Dados enviados! (simulação)");
+}
